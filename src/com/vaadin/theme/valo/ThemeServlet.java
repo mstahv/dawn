0488a462e999a919f6eeffb2f23c2d74bf418509
Jouni Koivuviita <jouni@jouni.me>
2014-03-03 16:46:22 +0200

    First commit
    Not much else than Button styled

---
+++ src/com/vaadin/theme/valo/ThemeServlet.java
@@ -0,0 +1,345 @@
+package com.vaadin.theme.valo;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.util.CurrentInstance;
+
+public class ThemeServlet extends VaadinServlet {
+	/**
+	 * Mutex for preventing to scss compilations to take place simultaneously.
+	 * This is a workaround needed as the scss compiler currently is not thread
+	 * safe (#10292).
+	 */
+	private static final Object SCSS_MUTEX = new Object();
+
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (isStaticResourceRequest(request)) {
+			// Define current servlet and service, but no request and response
+			getService().setCurrentInstances(null, null);
+			try {
+				serveStaticResources(request, response);
+				return;
+			} finally {
+				CurrentInstance.clearAll();
+			}
+		}
+		super.service(request, response);
+	}
+
+	private boolean serveStaticResources(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+
+		String pathInfo = request.getPathInfo();
+		if (pathInfo == null) {
+			return false;
+		}
+
+		if ((request.getContextPath() != null)
+				&& (request.getRequestURI().startsWith("/VAADIN/"))) {
+			serveStaticResourcesInVAADIN(request.getRequestURI(), request,
+					response);
+			return true;
+		} else if (request.getRequestURI().startsWith(
+				request.getContextPath() + "/VAADIN/")) {
+			serveStaticResourcesInVAADIN(
+					request.getRequestURI().substring(
+							request.getContextPath().length()), request,
+					response);
+			return true;
+		}
+
+		return false;
+	}
+
+	private URL findResourceURL(String filename, ServletContext sc)
+			throws MalformedURLException {
+		URL resourceUrl = sc.getResource(filename);
+		if (resourceUrl == null) {
+			// try if requested file is found from classloader
+
+			// strip leading "/" otherwise stream from JAR wont work
+			if (filename.startsWith("/")) {
+				filename = filename.substring(1);
+			}
+
+			resourceUrl = getService().getClassLoader().getResource(filename);
+		}
+		return resourceUrl;
+	}
+
+	private void serveStaticResourcesInVAADIN(String filename,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		final ServletContext sc = getServletContext();
+		URL resourceUrl = findResourceURL(filename, sc);
+
+		if (resourceUrl == null) {
+			// File not found, if this was a css request we still look for a
+			// scss file with the same name
+			if (serveOnTheFlyCompiledScss(filename, request, response, sc)) {
+				return;
+			} else {
+				// cannot serve requested file
+				getLogger()
+						.log(Level.INFO,
+								"Requested resource [{0}] not found from filesystem or through class loader."
+										+ " Add widgetset and/or theme JAR to your classpath or add files to WebContent/VAADIN folder.",
+								filename);
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			}
+			return;
+		}
+
+		// security check: do not permit navigation out of the VAADIN
+		// directory
+		if (!isAllowedVAADINResourceUrl(request, resourceUrl)) {
+			getLogger()
+					.log(Level.INFO,
+							"Requested resource [{0}] not accessible in the VAADIN directory or access to it is forbidden.",
+							filename);
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			return;
+		}
+		// Find the modification timestamp
+		long lastModifiedTime = 0;
+		URLConnection connection = null;
+		try {
+			connection = resourceUrl.openConnection();
+			lastModifiedTime = connection.getLastModified();
+			// Remove milliseconds to avoid comparison problems
+			// (milliseconds
+			// are not returned by the browser in the "If-Modified-Since"
+			// header).
+			lastModifiedTime = lastModifiedTime - lastModifiedTime % 1000;
+
+			if (browserHasNewestVersion(request, lastModifiedTime)) {
+				response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+				return;
+			}
+		} catch (Exception e) {
+			// Failed to find out last modified timestamp. Continue without
+			// it.
+			getLogger()
+					.log(Level.FINEST,
+							"Failed to find out last modified timestamp. Continuing without it.",
+							e);
+		} finally {
+			try {
+				// Explicitly close the input stream to prevent it
+				// from remaining hanging
+				// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4257700
+				InputStream is = connection.getInputStream();
+				if (is != null) {
+					is.close();
+				}
+			} catch (FileNotFoundException e) {
+				// Not logging when the file does not exist.
+			} catch (IOException e) {
+				getLogger().log(Level.INFO,
+						"Error closing URLConnection input stream", e);
+			}
+		}
+
+		// Set type mime type if we can determine it based on the filename
+		final String mimetype = sc.getMimeType(filename);
+		if (mimetype != null) {
+			response.setContentType(mimetype);
+		}
+
+		// Provide modification timestamp to the browser if it is known.
+		if (lastModifiedTime > 0) {
+			response.setDateHeader("Last-Modified", lastModifiedTime);
+			/*
+			 * The browser is allowed to cache for 1 hour without checking if
+			 * the file has changed. This forces browsers to fetch a new version
+			 * when the Vaadin version is updated. This will cause more requests
+			 * to the servlet than without this but for high volume sites the
+			 * static files should never be served through the servlet. The
+			 * cache timeout can be configured by setting the resourceCacheTime
+			 * parameter in web.xml
+			 */
+			int resourceCacheTime = getService().getDeploymentConfiguration()
+					.getResourceCacheTime();
+			String cacheControl = "max-age="
+					+ String.valueOf(resourceCacheTime);
+			if (filename.contains("nocache")) {
+				cacheControl = "public, max-age=0, must-revalidate";
+			}
+			response.setHeader("Cache-Control", cacheControl);
+		}
+
+		writeStaticResourceResponse(request, response, resourceUrl);
+	}
+
+	/**
+	 * Checks if the browser has an up to date cached version of requested
+	 * resource. Currently the check is performed using the "If-Modified-Since"
+	 * header. Could be expanded if needed.
+	 * 
+	 * @param request
+	 *            The HttpServletRequest from the browser.
+	 * @param resourceLastModifiedTimestamp
+	 *            The timestamp when the resource was last modified. 0 if the
+	 *            last modification time is unknown.
+	 * @return true if the If-Modified-Since header tells the cached version in
+	 *         the browser is up to date, false otherwise
+	 */
+	private boolean browserHasNewestVersion(HttpServletRequest request,
+			long resourceLastModifiedTimestamp) {
+		if (resourceLastModifiedTimestamp < 1) {
+			// We do not know when it was modified so the browser cannot
+			// have an
+			// up-to-date version
+			return false;
+		}
+		/*
+		 * The browser can request the resource conditionally using an
+		 * If-Modified-Since header. Check this against the last modification
+		 * time.
+		 */
+		try {
+			// If-Modified-Since represents the timestamp of the version
+			// cached
+			// in the browser
+			long headerIfModifiedSince = request
+					.getDateHeader("If-Modified-Since");
+
+			if (headerIfModifiedSince >= resourceLastModifiedTimestamp) {
+				// Browser has this an up-to-date version of the resource
+				return true;
+			}
+		} catch (Exception e) {
+			// Failed to parse header. Fail silently - the browser does not
+			// have
+			// an up-to-date version in its cache.
+		}
+		return false;
+	}
+
+	private boolean serveOnTheFlyCompiledScss(String filename,
+			HttpServletRequest request, HttpServletResponse response,
+			ServletContext sc) throws IOException {
+		if (!filename.endsWith(".css")) {
+			return false;
+		}
+
+		String scssFilename = filename.substring(0, filename.length() - 4)
+				+ ".scss";
+		URL scssUrl = findResourceURL(scssFilename, sc);
+		if (scssUrl == null) {
+			// Is a css request but no scss file was found
+			return false;
+		}
+		// security check: do not permit navigation out of the VAADIN
+		// directory
+		if (!isAllowedVAADINResourceUrl(request, scssUrl)) {
+			getLogger()
+					.log(Level.INFO,
+							"Requested resource [{0}] not accessible in the VAADIN directory or access to it is forbidden.",
+							filename);
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+			// Handled, return true so no further processing is done
+			return true;
+		}
+		if (getService().getDeploymentConfiguration().isProductionMode()) {
+			// This is not meant for production mode.
+			getLogger()
+					.log(Level.INFO,
+							"Request for {0} not handled by sass compiler while in production mode",
+							filename);
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			// Handled, return true so no further processing is done
+			return true;
+		}
+
+		// synchronized (SCSS_MUTEX) {
+		String realFilename = sc.getRealPath(scssFilename);
+		// ScssStylesheet scss = ScssStylesheet.get(realFilename);
+		// if (scss == null) {
+		// // Not a file in the file system (WebContent directory). Use
+		// // the
+		// // identifier directly (VAADIN/themes/.../styles.css) so
+		// // ScssStylesheet will try using the class loader.
+		// if (scssFilename.startsWith("/")) {
+		// scssFilename = scssFilename.substring(1);
+		// }
+		//
+		// scss = ScssStylesheet.get(scssFilename);
+		// }
+		//
+		// if (scss == null) {
+		// getLogger()
+		// .log(Level.WARNING,
+		// "Scss file {0} exists but ScssStylesheet was not able to find it",
+		// scssFilename);
+		// return false;
+		// }
+		String compiled = "";
+		try {
+			getLogger().log(Level.FINE, "Compiling {0} for request to {1}",
+					new Object[] { realFilename, filename });
+			compiled = Sass.compile(realFilename);
+		} catch (Exception e) {
+			getLogger().log(Level.WARNING, "Scss compilation failed", e);
+			return false;
+		}
+
+		// This is for development mode only so instruct the browser to
+		// never
+		// cache it
+		response.setHeader("Cache-Control", "no-cache");
+		final String mimetype = getService().getMimeType(filename);
+		writeResponse(response, mimetype, compiled);
+
+		return true;
+		// }
+	}
+
+	/**
+	 * Writes the response in {@code output} using the contentType given in
+	 * {@code contentType} to the provided {@link HttpServletResponse}
+	 * 
+	 * @param response
+	 * @param contentType
+	 * @param output
+	 *            Output to write (UTF-8 encoded)
+	 * @throws IOException
+	 */
+	private void writeResponse(HttpServletResponse response,
+			String contentType, String output) throws IOException {
+		response.setContentType(contentType);
+		final OutputStream out = response.getOutputStream();
+		// Set the response type
+		final PrintWriter outWriter = new PrintWriter(new BufferedWriter(
+				new OutputStreamWriter(out, "UTF-8")));
+		outWriter.print(output);
+		outWriter.flush();
+		outWriter.close();
+	}
+
+	private static final Logger getLogger() {
+		return Logger.getLogger(VaadinServlet.class.getName());
+	}
+
+}
---
+++ src/com/vaadin/theme/valo/ValoThemeUI.java
@@ -0,0 +1,257 @@
+package com.vaadin.theme.valo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+@Theme("valo-demo")
+@Title("Valo Theme")
+public class ValoThemeUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = ValoThemeUI.class)
+    public static class Servlet extends ThemeServlet {
+    }
+
+    CssLayout root = new CssLayout();
+    CssLayout components = new CssLayout();
+    CssLayout menu = new CssLayout();
+
+    HashMap<Category, ArrayList<Link>> menuItems = new HashMap<Category, ArrayList<Link>>();
+
+    enum Category {
+        Common, Basic_Components, Component_Containers, Selection_Components, Inputs, Feedback, Other
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        setContent(root);
+        root.addComponent(components);
+
+        components.addComponents(components(1), components(2), components(3),
+                components(4), components(5));
+        components.addStyleName("components-root");
+
+        root.addComponentAsFirst(buildMenu());
+
+    }
+
+    CssLayout buildMenu() {
+        menu.setPrimaryStyleName("valo-menu");
+
+        Label title = new Label("Valo Theme");
+        title.setSizeUndefined();
+        title.setPrimaryStyleName("valo-menu-title");
+        menu.addComponent(title);
+
+        final CheckBox enabled = new CheckBox("Enabled", true);
+        enabled.setPrimaryStyleName("valo-menu-enabled");
+        menu.addComponent(enabled);
+        enabled.addValueChangeListener(new ValueChangeListener() {
+            @Override
+            public void valueChange(ValueChangeEvent event) {
+                components.setEnabled(enabled.getValue());
+            }
+        });
+
+        for (Entry<Category, ArrayList<Link>> entry : menuItems.entrySet()) {
+            Label caption = new Label(entry.getKey().toString()
+                    .replaceAll("_", " "));
+            caption.setSizeUndefined();
+            caption.setPrimaryStyleName("valo-menu-category");
+            menu.addComponent(caption);
+
+            for (Link link : entry.getValue()) {
+                menu.addComponent(link);
+                link.setPrimaryStyleName("valo-menu-item");
+            }
+        }
+
+        return menu;
+    }
+
+    VerticalLayout components(int num) {
+        menuItems.clear();
+
+        final VerticalLayout root = new VerticalLayout();
+        root.setWidth("33.333%");
+        root.setMargin(true);
+        root.addStyleName("components");
+        root.addStyleName("color" + num);
+
+        /**
+         * Labels
+         */
+        addSection(root, "Labels", Category.Basic_Components);
+
+        Label h1 = new Label("Main Title");
+        h1.addStyleName("h1");
+        root.addComponent(h1);
+
+        Label large = new Label(
+                "Large type for introductory text. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu.");
+        large.addStyleName("large");
+        root.addComponent(large);
+
+        Label h2 = new Label("Subtitle");
+        h2.addStyleName("h2");
+        root.addComponent(h2);
+
+        Label normal = new Label(
+                "Normal type for plain text. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu.");
+        root.addComponent(normal);
+
+        Label h3 = new Label("Small Title");
+        h3.addStyleName("h3");
+        root.addComponent(h3);
+
+        Label small = new Label(
+                "Small type for additional text. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu.");
+        small.addStyleName("small");
+        root.addComponent(small);
+
+        Label h4 = new Label("Section Title");
+        h4.addStyleName("h4");
+        root.addComponent(h4);
+
+        normal = new Label(
+                "Normal type for plain text. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu.");
+        root.addComponent(normal);
+
+        /**
+         * Buttons
+         */
+        addSection(root, "Buttons", Category.Basic_Components);
+
+        HorizontalLayout row = new HorizontalLayout();
+        row.addStyleName("wrapping");
+        row.setSpacing(true);
+        root.addComponent(row);
+
+        Button button = new Button("Normal");
+        row.addComponent(button);
+
+        button = new Button("Focused");
+        button.addStyleName("focus");
+        row.addComponent(button);
+
+        button = new Button("Pressed");
+        button.addStyleName("active");
+        button.addStyleName("focus");
+        row.addComponent(button);
+
+        button = new Button("Disabled");
+        button.setEnabled(false);
+        row.addComponent(button);
+
+        button = new Button("Custom");
+        button.addStyleName("color2");
+        row.addComponent(button);
+
+        button = new Button("User Color");
+        button.addStyleName("color3");
+        row.addComponent(button);
+
+        button = new Button("Themed");
+        button.addStyleName("color4");
+        row.addComponent(button);
+
+        button = new Button("Alternate");
+        button.addStyleName("color5");
+        row.addComponent(button);
+
+        button = new Button("Other");
+        button.addStyleName("color6");
+        row.addComponent(button);
+
+        button = new Button("Small");
+        button.addStyleName("small");
+        row.addComponent(button);
+
+        button = new Button("Large");
+        button.addStyleName("large");
+        row.addComponent(button);
+
+        /**
+         * Text fields
+         */
+        addSection(root, "Text Fields", Category.Basic_Components);
+
+        row = new HorizontalLayout();
+        row.addStyleName("wrapping");
+        row.setSpacing(true);
+        root.addComponent(row);
+
+        TextField tf = new TextField("Normal");
+        tf.setInputPrompt("First name");
+        row.addComponent(tf);
+
+        tf = new TextField("Focused");
+        tf.setInputPrompt("Last name");
+        tf.addStyleName("focus");
+        row.addComponent(tf);
+
+        tf = new TextField("Custom color");
+        tf.setInputPrompt("Email");
+        tf.addStyleName("color1");
+        row.addComponent(tf);
+
+        tf = new TextField("Disabled");
+        tf.setInputPrompt("Gender");
+        tf.setEnabled(false);
+        row.addComponent(tf);
+
+        tf = new TextField("Themed (disabled)");
+        tf.setInputPrompt("Age");
+        tf.addStyleName("color2");
+        tf.setEnabled(false);
+        row.addComponent(tf);
+
+        tf = new TextField("Read-only");
+        tf.setInputPrompt("Nationality");
+        tf.setValue("Finnish");
+        tf.setReadOnly(true);
+        row.addComponent(tf);
+
+        return root;
+    }
+
+    void addSection(VerticalLayout root, String title, Category category) {
+        String id = title.toLowerCase().replace(" ", "-");
+
+        Label h1 = new Label(title);
+        h1.addStyleName("h1");
+        h1.setId("" + id);
+        root.addComponent(h1);
+
+        Link link = new Link(title, new ExternalResource("#" + id));
+
+        ArrayList<Link> items = menuItems.get(category);
+        if (items == null) {
+            items = new ArrayList<Link>();
+            menuItems.put(category, items);
+        }
+        items.add(link);
+    }
+
+}
