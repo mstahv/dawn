b10426c5b96a1f832246555e68cfee01daa7ee42
Jouni Koivuviita <jouni@jouni.me>
2014-04-09 22:16:07 +0300

    Fixed programmatic component focus inside windows
    Fixes issue #4

--- src/com/vaadin/theme/valo/test/TestUI.java
+++ src/com/vaadin/theme/valo/test/TestUI.java
@@ -1,10 +1,15 @@
 package com.vaadin.theme.valo.test;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
+@Theme("valo")
 public class TestUI extends UI {
 
     @Override
