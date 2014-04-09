b05e9f2345bbd0a280178f199a9b0c61dfa5111c
Jouni Koivuviita <jouni@jouni.me>
2014-04-09 22:04:09 +0300

    Removed Sass debug comments

--- src/com/vaadin/theme/valo/test/TestUI.java
+++ src/com/vaadin/theme/valo/test/TestUI.java
@@ -1,9 +1,6 @@
 package com.vaadin.theme.valo.test;
 
-import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -12,28 +9,21 @@ public class TestUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        VerticalLayout root = new VerticalLayout();
-        setContent(root);
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
 
-        TextField textfield = new TextField();
-        textfield.setIcon(new ThemeResource("../runo/icons/16/document.png"));
-        root.addComponent(textfield);
-
-        Table table = new Table();
-        table.setIcon(new ThemeResource("../runo/icons/16/document.png"));
-        root.addComponent(table);
-        table.addContainerProperty("Foo", String.class, null);
-        for (int i = 0; i < 1000; i++) {
-            table.addItem(new Object[] { "Name" + i }, i);
-        }
-
-        textfield = new TextField();
-        textfield.setIcon(new ThemeResource("../runo/icons/16/document.png"));
-        root.addComponent(textfield);
-
-        Label label = new Label("Label");
-        label.setIcon(new ThemeResource("../runo/icons/16/document.png"));
-        root.addComponent(label);
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                TextField textField = new TextField("KEnttä");
+                Window window = new Window();
+                window.setContent(new VerticalLayout(textField));
+                addWindow(window);
+                textField.focus();
+            }
+        });
+        layout.addComponent(button);
     }
 
 }
