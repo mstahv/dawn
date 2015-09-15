213beaaa107f4a50d689bcff166adad60025924e
Matti Tahvonen <matti@vaadin.com>
2015-09-15 22:33:33 +0300

    Hide errors publishing cdn fonts (happens disturbingly on portals)

--- src/main/java/org/peimari/dawn/CdnFonts.java
+++ src/main/java/org/peimari/dawn/CdnFonts.java
@@ -8,56 +8,73 @@ import com.vaadin.server.SessionInitEvent;
 import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinServletService;
 import com.vaadin.server.VaadinSession;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-
 @WebListener
 public class CdnFonts implements javax.servlet.http.HttpSessionListener {
-    private boolean inited = "true".equals(System.getProperty("skipDawnCloudFonts"));
-
-    public CdnFonts() { 
+    
+    private boolean inited = "true".equals(System.getProperty(
+            "skipDawnCloudFonts"));
+    
+    public CdnFonts() {
     }
-
+    
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        if(!inited) {
-            VaadinServletService.getCurrent().addSessionInitListener(new SessionInitListener() {
-
-                @Override
-                public void sessionInit(SessionInitEvent event) throws ServiceException {
-            VaadinSession.getCurrent().addBootstrapListener(new BootstrapListener() {
-
-                @Override
-                public void modifyBootstrapFragment(
-                        BootstrapFragmentResponse response) {
-                }
-
-                @Override
-                public void modifyBootstrapPage(BootstrapPageResponse response) {
-                // Update the bootstrap page
-                    Document document = response.getDocument();
-                    Element head = document.getElementsByTag("head").first();
-                    Element link = document.createElement("link");
-                    link.attr("href", "//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700");
-                    link.attr("rel", "stylesheet");
-                    link.attr("type", "text/css");
-                    head.appendChild(link);
-                    link = document.createElement("link");
-                    link.attr("href", "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css");
-                    link.attr("rel", "stylesheet");
-                    link.attr("type", "text/css");
-                    head.appendChild(link);
-                }
-            });
-            inited = true;
-                }
-            });
+        if (!inited) {
+            try {
+                
+                VaadinServletService.getCurrent().addSessionInitListener(
+                        new SessionInitListener() {
+                    
+                    @Override
+                    public void sessionInit(SessionInitEvent event) throws ServiceException {
+                        VaadinSession.getCurrent().addBootstrapListener(
+                                new BootstrapListener() {
+                            
+                            @Override
+                            public void modifyBootstrapFragment(
+                                    BootstrapFragmentResponse response) {
+                            }
+                            
+                            @Override
+                            public void modifyBootstrapPage(
+                                    BootstrapPageResponse response) {
+                                // Update the bootstrap page
+                                Document document = response.getDocument();
+                                Element head = document.getElementsByTag("head").
+                                        first();
+                                Element link = document.createElement("link");
+                                link.attr("href",
+                                        "//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700");
+                                link.attr("rel", "stylesheet");
+                                link.attr("type", "text/css");
+                                head.appendChild(link);
+                                link = document.createElement("link");
+                                link.attr("href",
+                                        "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css");
+                                link.attr("rel", "stylesheet");
+                                link.attr("type", "text/css");
+                                head.appendChild(link);
+                            }
+                        });
+                        inited = true;
+                    }
+                });
+            } catch (Exception e) {
+                // This happens in e.g. Portal, but is typically not critical.
+                Logger.getLogger(CdnFonts.class.getName()).log(Level.FINE,
+                        "Could not set cdn font files",
+                        e);
+            }
         }
     }
-
+    
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
     }
